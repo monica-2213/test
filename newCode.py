4ae29719bfa1759e252931c368080e02d6011b92
EVANGELINE MONICA A/P RAJEN <93172320+monica-2213@users.noreply.github.com>
2023-05-11 00:12:46 +0800

    Update newCode.py

--- newCode.py
+++ newCode.py
@@ -52,7 +52,7 @@ else:
     hormonal_contraceptives_years = 0
 iud = st.selectbox("Have you ever used an intrauterine device (IUD)?", ("Yes", "No"))
 if iud == "Yes":
-    iud_years = st.number_input("For how many years have you used an IUD?", min_value=1)
+    iud_years = st.number_input("For how many years have you used an IUD?", min_value=1, value=1)
 else:
     iud_years = 0
 stds = st.selectbox("Have you ever had a sexually transmitted disease?", ("Yes", "No"))
@@ -62,5 +62,26 @@ else:
     stds_number = 0
 
 if st.button("Submit"):
-    result = expert_system(age, number_of_sexual_partners, first_sexual_intercourse, num_pregnancies, smokes, smokes_years, smokes_packs_year, hormonal_contraceptives, hormonal_contraceptives_years, iud, iud_years, stds, stds_number)
-    st.write(result)
+    # Convert binary categorical variables to numerical
+    X_test["Smokes"] = X_test["Smokes"].map({"yes": 1, "no": 0})
+    X_test["Hormonal Contraceptives"] = X_test["Hormonal Contraceptives"].map({"yes": 1, "no": 0})
+    X_test["IUD"] = X_test["IUD"].map({"yes": 1, "no": 0})
+    X_test["STDs"] = X_test["STDs"].map({"yes": 1, "no": 0})
+
+    # Convert input data to numerical values
+    smokes_num = 1 if smokes == "Yes" else 0
+    hormonal_contraceptives_num = 1 if hormonal_contraceptives == "Yes" else 0
+    iud_num = 1 if iud == "Yes" else 0
+    stds_num = 1 if stds == "Yes" else 0
+
+    # Create input data array
+    input_data = np.array([age, number_of_sexual_partners, first_sexual_intercourse, num_pregnancies, smokes_num, smokes_years, smokes_packs_year, hormonal_contraceptives_num, hormonal_contraceptives_years, iud_num, iud_years, stds_num, stds_number]).reshape(1, -1)
+
+    # Make prediction
+    prediction = clf.predict(input_data)
+
+    # Show result
+    if prediction[0] == 0:
+        st.write("You are not at risk of cervical cancer.")
+    else:
+        st.write("You may be at risk of cervical cancer. Please consult a doctor.")
