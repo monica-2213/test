77d0732581373592d79adbda30304c7980836d3f
EVANGELINE MONICA A/P RAJEN <93172320+monica-2213@users.noreply.github.com>
2023-05-11 01:29:18 +0800

    Update newCode.py

--- newCode.py
+++ newCode.py
@@ -1,168 +1,89 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
+import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
-# use the numpy scalar type
-x = np.array([True, False], dtype=np.bool_)
+# Load dataset
+url = 'https://datahub.io/machine-learning/cervical-cancer/r/cervical-cancer.csv'
+data = pd.read_csv(url)
 
-data_url = 'https://datahub.io/machine-learning/cervical-cancer/r/cervical-cancer.csv'
-df = pd.read_csv(data_url)
+# Split dataset into features and target
+X = data.drop(['Biopsy'], axis=1)
+y = data['Biopsy']
 
-# Display the dataframe
-st.write(df)
+# Split dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Define the rules library
-rules = {
-    'age': {
-        'rule': lambda x: x >= 18 and x <= 84,
-        'error_message': 'Please enter a valid age between 18 and 84'
-    },
-    'num_sexual_partners': {
-        'rule': lambda x: x >= 0,
-        'error_message': 'Please enter a valid number of sexual partners'
-    },
-    'first_sexual_intercourse': {
-        'rule': lambda x: x >= 10 and x <= 32,
-        'error_message': 'Please enter a valid age of first sexual intercourse between 10 and 32'
-    },
-    'num_pregnancies': {
-        'rule': lambda x: x >= 0,
-        'error_message': 'Please enter a valid number of pregnancies'
-    },
-    'smoke': {
-        'rule': lambda x: x in ['Yes', 'No'],
-        'error_message': 'Please enter either Yes or No for smoking'
-    },
-    'smoking_years': {
-        'rule': lambda x: x >= 0,
-        'error_message': 'Please enter a valid number of smoking years'
-    },
-    'hormonal_contraceptives': {
-        'rule': lambda x: x in ['Yes', 'No'],
-        'error_message': 'Please enter either Yes or No for hormonal contraceptives'
-    },
-    'hormonal_contraceptives_years': {
-        'rule': lambda x: x >= 0,
-        'error_message': 'Please enter a valid number of hormonal contraceptive years'
-    },
-    'iud': {
-        'rule': lambda x: x in ['Yes', 'No'],
-        'error_message': 'Please enter either Yes or No for intrauterine device (IUD)'
-    },
-    'iud_years': {
-        'rule': lambda x: x >= 0,
-        'error_message': 'Please enter a valid number of intrauterine device (IUD) years'
-    },
-    'stds': {
-        'rule': lambda x: x in ['Yes', 'No'],
-        'error_message': 'Please enter either Yes or No for sexually transmitted diseases (STDs)'
-    },
-    'num_stds': {
-        'rule': lambda x: x >= 0,
-        'error_message': 'Please enter a valid number of sexually transmitted disease diagnoses'
-    },
-    'time_since_first_std': {
-        'rule': lambda x: x >= 0,
-        'error_message': 'Please enter a valid time (in years) since first STD diagnosis'
-    },
-    'time_since_last_std': {
-        'rule': lambda x: x >= 0,
-        'error_message': 'Please enter a valid time (in years) since last STD diagnosis'
-    },
-}
+# Train Random Forest classifier
+rfc = RandomForestClassifier(n_estimators=100, random_state=42)
+rfc.fit(X_train, y_train)
 
-def cervical_cancer_rules(age, num_sexual_partners, first_sexual_intercourse, num_pregnancies, smoking, smoking_years, hormonal_contraceptives, hormonal_contraceptives_years, iud, iud_years, std, num_std_diagnoses, time_since_first_std, time_since_last_std):
-    risk = 0
-    
-    # Rule 1: Age over 21 years old increases risk
-    if age > 21:
-        risk += 1
-    
-    # Rule 2: Having more than 3 sexual partners increases risk
-    if num_sexual_partners > 3:
-        risk += 1
-    
-    # Rule 3: Early age at first sexual intercourse (less than 16 years old) increases risk
-    if first_sexual_intercourse < 16:
-        risk += 1
-    
-    # Rule 4: Number of pregnancies over 3 increases risk
-    if num_pregnancies > 3:
-        risk += 1
-    
-    # Rule 5: Smoking increases risk
-    if smoking == 'Yes':
-        risk += 1
-        if smoking_years >= 15:
-            risk += 1
-    
-    # Rule 6: Use of hormonal contraceptives increases risk
-    if hormonal_contraceptives == 'Yes':
-        risk += 1
-        if hormonal_contraceptives_years >= 5:
-            risk += 1
-    
-    # Rule 7: Use of an IUD decreases risk
-    if iud == 'Yes':
-        risk -= 1
-        if iud_years >= 3:
-            risk -= 1
-    
-    # Rule 8: Having a history of STDs increases risk
-    if std == 'Yes':
-        risk += 1
-        if num_std_diagnoses > 1:
-            risk += 1
-        if time_since_first_std < 5:
-            risk += 1
-        if time_since_last_std < 5:
-            risk += 1
-    
-    return risk
+# Evaluate model on testing set
+y_pred = rfc.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
 
-def app():
-    # Set the title of the web app
-    st.title('Cervical Cancer Risk Calculator')
+# Create Streamlit app
+st.title("Cervical Cancer Diagnosis Expert System")
+st.write("This system predicts the likelihood of cervical cancer based on patient characteristics.")
 
-    # Create input fields for each variable
-    age = st.number_input('Age', min_value=18, max_value=84, value=25)
-    num_sexual_partners = st.number_input('Number of Sexual Partners', min_value=0, value=1)
-    first_sexual_intercourse = st.number_input('Age at First Sexual Intercourse', min_value=10, max_value=32, value=16)
-    num_pregnancies = st.number_input('Number of Pregnancies', min_value=0, value=1)
-    smoking = st.selectbox('Do you smoke?', ['Yes', 'No'])
-    if smoking == 'Yes':
-        smoking_years = st.number_input('How many years have you been smoking?', min_value=0, value=1)
+# Create form for user input
+age = st.slider("Age", 14, 84, 25)
+number_of_sexual_partners = st.slider("Number of Sexual Partners", 0, 28, 1)
+first_sexual_intercourse = st.slider("Age of First Sexual Intercourse", 10, 40, 16)
+num_pregnancies = st.slider("Number of Pregnancies", 0, 11, 0)
+smokes = st.selectbox("Smoking Status", ["Never", "Former", "Current"])
+smokes_years = st.slider("Years of Smoking", 0, 50, 0)
+smokes_packs = st.slider("Packs of Smoking per Year", 0, 100, 0)
+hormonal_contraceptives = st.selectbox("Hormonal Contraceptives", ["Yes", "No"])
+iud = st.selectbox("IUD", ["Yes", "No"])
+stds = st.selectbox("STDs", ["Yes", "No"])
+stds_number = st.slider("Number of STDs", 0, 7, 0)
+stds_condylomatosis = st.selectbox("Condylomatosis", ["Yes", "No"])
+stds_cervical_condylomatosis = st.selectbox("Cervical Condylomatosis", ["Yes", "No"])
+stds_vaginal_condylomatosis = st.selectbox("Vaginal Condylomatosis", ["Yes", "No"])
+stds_vulvo_perineal_condylomatosis = st.selectbox("Vulvo-perineal Condylomatosis", ["Yes", "No"])
+stds_syphilis = st.selectbox("Syphilis", ["Yes", "No"])
+stds_pelvic_inflammatory_disease = st.selectbox("Pelvic Inflammatory Disease", ["Yes", "No"])
+stds_genital_herpes = st.selectbox("Genital Herpes", ["Yes", "No"])
+stds_molluscum_contagiosum = st.selectbox("Molluscum Contagiosum", ["Yes", "No"])
+stds_aids = st.selectbox("AIDS", ["Yes", "No"])
+stds_hiv = st.selectbox("HIV", ["Yes", "No"])
+stds_hepatitis_b = st.selectbox("Hepatitis B", ["Yes", "No"])
+
+# Create prediction button
+if st.button("Predict"):
+    # Create input data
+    input_data = {
+        'Age': age,
+        'Number of sexual partners': number_of_sexual_partners,
+        'First sexual intercourse': first_sexual_intercourse,
+        'Num of pregnancies': num_pregnancies,
+        'Smokes': 1 if smokes == "Former" or smokes == "Current" else 0,
+        'Smokes (years)': smokes_years,
+        'Smokes (packs/year)': smokes_packs,
+        'Hormonal Contraceptives': 1 if hormonal_contraceptives == "Yes" else 0,
+        'IUD': 1 if iud == "Yes" else 0,
+        'STDs': 1 if stds == "Yes" else 0,
+        'STDs (number)': stds_number,
+        'STDs:condylomatosis': 1 if stds_condylomatosis == "Yes" else 0,
+        'STDs:cervical condylomatosis': 1 if stds_cervical_condylomatosis == "Yes" else 0,
+        'STDs:vaginal condylomatosis': 1 if stds_vaginal_condylomatosis == "Yes" else 0,
+        'STDs:vulvo-perineal condylomatosis': 1 if stds_vulvo_perineal_condylomatosis == "Yes" else 0,
+        'STDs:syphilis': 1 if stds_syphilis == "Yes" else 0,
+        'STDs:pelvic inflammatory disease': 1 if stds_pelvic_inflammatory_disease == "Yes" else 0,
+        'STDs:genital herpes': 1 if stds_genital_herpes == "Yes" else 0,
+        'STDs:molluscum contagiosum': 1 if stds_molluscum_contagiosum == "Yes" else 0,
+        'STDs:AIDS': 1 if stds_aids == "Yes" else 0,
+        'STDs:HIV': 1 if stds_hiv == "Yes" else 0,
+        'STDs:hepatitis B': 1 if stds_hepatitis_b == "Yes" else 0
+    }
+
+    # Make prediction
+    prediction = rfc.predict([list(input_data.values())])[0]
+
+    # Display prediction
+    if prediction == 0:
+        st.write("The patient is likely not to have cervical cancer.")
     else:
-        smoking_years = 0
-    hormonal_contraceptives = st.selectbox('Do you use hormonal contraceptives?', ['Yes', 'No'])
-    if hormonal_contraceptives == 'Yes':
-        hormonal_contraceptives_years = st.number_input('How many years have you been using hormonal contraceptives?', min_value=0, value=1)
-    else:
-        hormonal_contraceptives_years = 0
-    iud = st.selectbox('Do you use an intrauterine device (IUD)?', ['Yes', 'No'])
-    if iud == 'Yes':
-        iud_years = st.number_input('How many years have you been using an IUD?', min_value=0, value=1)
-    else:
-        iud_years = 0
-    std = st.selectbox('Have you been diagnosed with a sexually transmitted disease (STD)?', ['Yes', 'No'])
-    if std == 'Yes':
-        num_std_diagnoses = st.number_input('How many STD diagnoses have you had?', min_value=0, value=1)
-        time_since_first_std = st.number_input('How many years ago was your first STD diagnosis?', min_value=0, value=1)
-        time_since_last_std = st.number_input('How many years ago was your last STD diagnosis?', min_value=0, value=1)
-    else:
-        num_std_diagnoses = 0
-        time_since_first_std = 0
-        time_since_last_std = 0
-
-    # Check if the input values meet the rules
-    error_message = ''
-    for variable, value in zip(['age', 'num_sexual_partners', 'first_sexual_intercourse', 'num_pregnancies', 'smoke', 'smoking_years', 'hormonal_contraceptives', 'hormonal_contraceptives_years', 'iud', 'iud_years', 'stds', 'num_stds', 'time_since_first_std', 'time_since_last_std'], [age, num_sexual_partners, first_sexual_intercourse, num_pregnancies, smoking, smoking_years, hormonal_contraceptives, hormonal_contraceptives_years, iud, iud_years, std, num_std_diagnoses, time_since_first_std, time_since_last_std]):
-        if not rules[variable]['rule'](value):
-            error_message += rules[variable]['error_message'] + '\n'
-
-    # If there is an error, display the error message
-    if error_message:
-        st.error(error_message)
-    else:
-        # Calculate the risk of cervical cancer based on the input values
-        risk = cervical_cancer_rules
+        st.write("The patient is likely to have cervical cancer.")
